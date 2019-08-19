e6ffe19f8dfe1663418bb91620ce03448058d34e
Tim Baldwin <tim@purestake.com>
2019-08-19 09:11:40 -0400

    Added new GET transaction By Id

---
+++ javascript-examples/get_tx.js
@@ -0,0 +1,22 @@
+// GET Transaction 
+// https://www.purestake.com and https://developer.purestake.io
+// 
+// Code borrowed and extended from https://developer.algorand.org/docs/javascript-sdk
+//
+// Example using PureStake token object 
+//
+
+const algosdk = require('algosdk');
+const baseServer = "https://testnet-algorand.api.purestake.io/ps1"
+const port = "";
+const token = {
+    'X-API-Key': 'B3SU4KcVKi94Jap2VXkK83xx38bsv95K5UZm2lab'
+}
+
+const algodclient = new algosdk.Algod(token, baseServer, port);
+
+(async () => {
+    console.log(await algodclient.transactionById('3J7Z46VKGJ6VATCHOE3I2JZXVDXKAWOKO3JJCR6R7EEX5VBKEATQ'));
+})().catch(e => {
+    console.log(e);
+});
