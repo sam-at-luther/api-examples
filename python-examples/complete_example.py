51d8db67cc1ec543b4c9e6ace4d1fa19dbbfe8ab
Tim Baldwin <tim@purestake.com>
2020-04-09 08:19:33 -0400

    Updated to include verification

--- python-examples/complete_example.py
+++ python-examples/complete_example.py
@@ -6,19 +6,21 @@ from algosdk import account
 from algosdk import mnemonic
 import json
 
-# Initalize throw-away account for this example - check that is has funds before running script
-mnemonic_phrase = "code thrive mouse code badge example pride stereo sell viable adjust planet text close erupt embrace nature upon february weekend humble surprise shrug absorb faint"; 
-account_private_key = mnemonic.to_private_key(mnemonic_phrase)
-account_public_key = mnemonic.to_public_key(mnemonic_phrase)
 
 # Setup HTTP client w/guest key provided by PureStake
 algod_token = 'B3SU4KcVKi94Jap2VXkK83xx38bsv95K5UZm2lab'
 algod_address = 'https://testnet-algorand.api.purestake.io/ps1'
 purestake_token = {'X-Api-key': algod_token}
-acl = algod.AlgodClient(algod_token, algod_address, headers=purestake_token)
+
+# Initalize throw-away account for this example - check that is has funds before running script
+mnemonic_phrase = "code thrive mouse code badge example pride stereo sell viable adjust planet text close erupt embrace nature upon february weekend humble surprise shrug absorb faint"; 
+account_private_key = mnemonic.to_private_key(mnemonic_phrase)
+account_public_key = mnemonic.to_public_key(mnemonic_phrase)
+
+algodclient = algod.AlgodClient(algod_token, algod_address, headers=purestake_token)
 
 # get suggested parameters from Algod
-params = acl.suggested_params()
+params = algodclient.suggested_params()
 gen = params["genesisID"]
 gh = params["genesishashb64"]
 first_valid_round = params["lastRound"]
@@ -33,10 +35,22 @@ send_to_address = 'AEC4WDHXCDF4B5LBNXXRTB3IJTVJSWUZ4VJ4THPU2QGRJGTA3MIDFN3CQA'
 tx = transaction.PaymentTxn(existing_account, fee, first_valid_round, last_valid_round, gh, send_to_address, send_amount)
 signed_tx = tx.sign(account_private_key)
 
+# Function from Algorand Inc. - utility for waiting on a transaction confirmation
+def wait_for_confirmation( algodclient, txid ):
+    last_round = algodclient.status().get('lastRound')
+    while True:
+        txinfo = algodclient.pending_transaction_info(txid)
+        if txinfo.get('round') and txinfo.get('round') > 0:
+           print("Transaction {} confirmed in round {}.".format(txid, txinfo.get('round')))
+           break
+        else:
+           print("Waiting for confirmation...")
+           last_round += 1
+           algodclient.status_after_block(last_round)
 
 try:
-    tx_confirm = acl.send_transaction(signed_tx, headers={'content-type': 'application/x-binary'})
-    print(tx_confirm)
+    tx_confirm = algodclient.send_transaction(signed_tx, headers={'content-type': 'application/x-binary'})
+    wait_for_confirmation(algodclient, txid = signed_tx.transaction.get_txid())
 except Exception as e:
     print(e)
 
