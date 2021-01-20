67b8fbd3d58f1976dafded964b14c619cb08d3c4
Tim Baldwin <tim@purestake.com>
2021-01-20 14:36:11 -0500

    Fixed logic sig example for v2

--- javascript-examples/logicSig.js
+++ javascript-examples/logicSig.js
@@ -15,67 +15,28 @@
 
 
 const algosdk = require('algosdk');
-const baseServer = "https://testnet-algorand.api.purestake.io/ps1"
+const baseServer = "https://testnet-algorand.api.purestake.io/ps2"
 const port = "";
-
-// Create client for transaction POST
-const postToken = {
-    'X-API-key' : 'B3SU4KcVKi94Jap2VXkK83xx38bsv95K5UZm2lab',
-    'Content-Type' : 'application/x-binary'
-}
-
-const postAlgodclient = new algosdk.Algod(postToken, baseServer, port); // Binary content type
-
-//Create client for GET of Transaction parameters 
 const token = {
-    'X-API-key' : 'B3SU4KcVKi94Jap2VXkK83xx38bsv95K5UZm2lab',
+    'X-API-key': 'B3SU4KcVKi94Jap2VXkK83xx38bsv95K5UZm2lab',
 }
 
 
-const algodclient = new algosdk.Algod(token, baseServer, port); 
+const algodclient = new algosdk.Algodv2(token, baseServer, port);
 
-var mnemonic = "code thrive mouse code badge example pride stereo sell viable adjust planet text close erupt embrace nature upon february weekend humble surprise shrug absorb faint"; 
-var recoveredAccount = algosdk.mnemonicToSecretKey(mnemonic); 
+var mnemonic = "code thrive mouse code badge example pride stereo sell viable adjust planet text close erupt embrace nature upon february weekend humble surprise shrug absorb faint";
+var recoveredAccount = algosdk.mnemonicToSecretKey(mnemonic);
 console.log(recoveredAccount.addr);
 
-//instantiate the algod wrapper
-
-//submit the transaction
-(async() => {
+(async () => {
     //Get the relevant params from the algod
-    let params = await algodclient.getTransactionParams();
-    console.log("here" + params);
-    let endRound = params.lastRound + parseInt(1000);
-    let fee = await algodclient.suggestedFee();
+    let params = await algodclient.getTransactionParams().do();
 
     // move the TEAL  program into Uint8Array
     let program = new Uint8Array(Buffer.from("ASABASI=", "base64"));
-
-    // makeLogicSig method takes the program and parameters
-    // in this example we have no parameters
-    // If we did have parameters you would add them like
-    // let args = [
-    //    Uint8Array.from("123"),
-    //    Uint8Array.from("456")
-    // ];
-    // And remember TEAL parameters are order specific
     let lsig = algosdk.makeLogicSig(program);
-    
-    // sign the logic with your accounts secret
-    // key. This is essentially giving your
-    // key authority to anyone with the lsig
-    // and if the logic returns true
-    // exercise extreme care
-    // If this were an escrow account usage
-    // you would not do this sign operation
     lsig.sign(recoveredAccount.sk);
 
-    // At this point you can save the lsig off and share
-    // as your delegated signature.
-    // The LogicSig class supports serialization and
-    // provides the lsig.toByte and fromByte methods
-    // to easily convert for file saving and 
-    // reconstituting and LogicSig object
 
     //create a transaction
     let txn = {
@@ -83,19 +44,17 @@ console.log(recoveredAccount.addr);
         "to": "SOEI4UA72A7ZL5P25GNISSVWW724YABSGZ7GHW5ERV4QKK2XSXLXGXPG5Y",
         "fee": params.fee,
         "amount": 100000,
-        "firstRound": params.lastRound,
-        "lastRound": endRound,
+        "firstRound": params.firstRound,
+        "lastRound": params.lastRound,
         "genesisID": params.genesisID,
-        "genesisHash": params.genesishashb64
+        "genesisHash": params.genesisHash,
     };
 
-    // create logic signed transaction.
-    // Had this been an escrow the lsig would not contain the
-    // signature but would be submitted the same way
+
     let rawSignedTxn = algosdk.signLogicSigTransaction(txn, lsig);
 
     //Submit the lsig signed transaction
-    let tx = (await postAlgodclient.sendRawTransaction(rawSignedTxn.blob));
+    let tx = await algodclient.sendRawTransaction(rawSignedTxn.blob).do();
     console.log("Transaction : " + tx.txId);
 
 })().catch(e => {
